7d23b31e9009dd1fd56cacff0ff424c9f1ab7c71
Kirucoderanger <klemu@byupathway.edu>
2026-02-08 04:54:47 +0300

    requier to import

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+/*
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -38,3 +37,39 @@ app.use("/api", require("./routes/foodRoutes"));
 app.listen(5000, () =>
   console.log("Server running on port 5000")
 );
+*/
+
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+import cartRoutes from "./routes/cartRoutes.js";
+import restaurantRoutes from "./routes/restaurantRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import foodRoutes from "./routes/foodRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/cart", cartRoutes);
+app.use("/api/restaurants", restaurantRoutes);
+app.use("/api", userRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", foodRoutes);
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
