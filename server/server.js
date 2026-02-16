b84bb0a1ccea4e3c6a918032b61e292cebe21e8c
Kirucoderanger <klemu@byupathway.edu>
2026-02-17 01:12:48 +0300

    log process.env.JWT_SECRET

--- server/server.js
+++ server/server.js
@@ -113,6 +113,9 @@ if (!process.env.JWT_SECRET || process.env.JWT_SECRET.length < 32) {
   process.exit(1);
 }
 
+console.log("JWT_SECRET on startup:", process.env.JWT_SECRET);
+
+
 dotenv.config();
 connectDB();
 
