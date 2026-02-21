ca6f02dced9d4cac3b55d717ef420f2bc1365afb
Kirucoderanger <klemu@byupathway.edu>
2026-02-21 22:29:31 +0300

    get all foods for restaurant added

--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -96,7 +96,7 @@ export default router;
 
 */
 
-
+/*
 import express from "express";
 import Restaurant from "../models/Restaurant.js";
 
@@ -114,3 +114,31 @@ router.get("/restaurants/:restaurantId", async (req, res) => {
 
 export default router;
 
+*/
+
+import express from "express";
+import Restaurant from "../models/Restaurant.js";
+import Food from "../models/Food.js";
+const router = express.Router();
+
+// Get all restaurants
+router.get("/", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find();
+    res.json(restaurants);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get all foods for a restaurant
+router.get("/:id/foods", async (req, res) => {
+  try {
+    const foods = await Food.find({ restaurant: req.params.id });
+    res.json(foods);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
